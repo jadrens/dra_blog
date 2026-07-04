ba1a78869bd7a7ea777af1894b2605746456fa20
DragonRen <dragonren@dragonren.top>
2026-07-04 13:59:13 +0000

    fix the build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: "build", 
+  experimental: {
+    workerThreads: process.env.CI ? false : undefined,
+    cpus: process.env.CI ? 1 : undefined,
+  },
 };
 
 export default nextConfig;

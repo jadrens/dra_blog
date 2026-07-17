e246bf3f491ecbd80339053e73d3b85d60d6f656
DragonRen <dragonren@dragonren.top>
2026-07-17 20:22:23 +0900

    update website name

--- src/var/config.ts
+++ src/var/config.ts
@@ -1,6 +1,6 @@
 export const SITE_CONFIG = {
   baseUrl: "https://blog.jadren.me",
-  siteName: "rayoumeu blog",
+  siteName: "jadren blog",
   description: "A blog with markdown and LaTeX support",
   githubRepo: "https://github.com/jadrens/drablog",
   githubBranch: "master",
